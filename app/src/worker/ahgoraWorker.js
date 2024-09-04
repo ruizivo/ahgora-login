c7c5a7b6ab51d7c66a3c615777f039256560b142
Rui Menezes <ruizivo.menezes@gmail.com>
2024-09-04 08:53:10 -0300

    correção no mes

--- app/src/worker/ahgoraWorker.js
+++ app/src/worker/ahgoraWorker.js
@@ -6,7 +6,25 @@ export function hello(name) {
 }
 
 
-export async function consultaPonto(year, month, force=false) {
-    return await AppService.espelhoPonto(year,month, force);
-  }
-  
+export async function consultaPonto(year, month, force = false) {
+  const anoMesAnterior = obterMesAnterior(year, month);
+
+  let mesAnterior = await AppService.espelhoPonto(
+    anoMesAnterior.year,
+    anoMesAnterior.month,
+    force
+  );
+  let mesAtual = await AppService.espelhoPonto(year, month, force);
+  mesAtual.dias = { ...mesAnterior.dias, ...mesAtual.dias };
+  return mesAtual;
+}
+
+function obterMesAnterior(ano, mes) {
+  const data = new Date(ano, mes - 1);
+  data.setMonth(data.getMonth() - 1);
+
+  const mesAnterior = data.getMonth() + 1;
+  const anoAnterior = data.getFullYear();
+
+  return { year: anoAnterior, month: mesAnterior };
+}
